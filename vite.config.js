9c788b6921960c9c575ed30bffc91ac063d06c39
arvindyadv03 <106082787+arvindyadv03@users.noreply.github.com>
2025-04-03 12:42:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: "/",
-    server: {
-      
-        historyApiFallback: true,
-    },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
+  plugins: [react()],
+  server: {
+    port: 3000, // Optional, set your preferred port
+  },
+  build: {
+    outDir: 'dist',
+  },
+  base: '/', // Ensure the base path is correct
 });
