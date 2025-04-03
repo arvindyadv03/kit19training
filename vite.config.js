91b7a3cb86d3d9d31380891e0954bb935fdf08d8
Arvind <arvind24042003@gmail.com>
2025-04-03 11:29:02 +0530

    Updated files

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/",
     server: {
         port: 3000,
+        historyApiFallback: true,
     },
     resolve: {
         alias: {
